8f618fc0d4ce9476d6cc86ffde28956d3cb752c3
殷泰山 <yintaishan@xkw.cn>
2016-11-28 23:38:21 +0800

    Android Studio Commit :提交MainActivity 和activity_main.xml

--- app/src/main/java/com/example/diudiu/myfirstgithubproject/MainActivity.java
+++ app/src/main/java/com/example/diudiu/myfirstgithubproject/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.diudiu.myfirstgithubproject;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.button = (Button) findViewById(R.id.button);
+        this.textView = (TextView) findViewById(R.id.textView);
+
+        this.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText("button clicked!!!");
+            }
+        });
+
     }
 }
